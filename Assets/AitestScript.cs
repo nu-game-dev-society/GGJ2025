a8a5ce61113072026381fdb3b11bd1fdf0bb6e2b
gamejam <gamejam@C22000030>
2025-01-25 19:45:05 +0000

    not quite lol

--- Assets/AitestScript.cs
+++ Assets/AitestScript.cs
@@ -3,45 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Splines;
 
-public class AiInputController : InputController
+public class AitestScript : MonoBehaviour
 {
-    public SplineContainer splineContainer;
-
-    public float t = 0.0f;
-    public float tStep = 1.0f;
-    public float acceptanceV = 3.0f;
-
+    public Transform target;
 
     void Start()
     {
     }
-    Vector3 pos;
-    Vector3 target;
-    Vector3 dir;
+    public Vector3 dir;
     public Vector3 fwdCross;
+    public Vector3 rightCross;
+    public Vector3 upCross;
 
+
+    public Vector3 currentSteerRequest;
+
+    public bool backwards;
     public string statusFwd;
     public string statusPtch;
 
     void Update()
     {
-        pos = transform.position;
-        target = splineContainer.EvaluatePosition(t);
-        if (Vector3.Distance(pos, target) < acceptanceV)
-        {
-            t += tStep;
-        }
 
-        dir = (target - pos).normalized;
+        dir = (target.position - transform.position).normalized;
 
 
         currentSteerRequest = Vector3.zero;
 
         fwdCross = Vector3.Cross(transform.forward, dir);
-        Vector3 rightCross = Vector3.Cross(transform.right, dir);
-        Vector3 upCross = Vector3.Cross(transform.up, dir);
-
-
+        rightCross = Vector3.Cross(transform.right, dir);
+        upCross = Vector3.Cross(transform.up, dir);
 
         if (fwdCross.y > 0.0f && rightCross.y > 0.0f)
         {
@@ -62,36 +53,39 @@ public class AiInputController : InputController
 
         if (fwdCross.x > 0.0f && upCross.x > 0.0f)
         {
-            fwdCross.x = 1.0f;
+            fwdCross.x = -1.0f;
             statusPtch = "BR";
         }
         else if (fwdCross.x < 0.0f && upCross.x > 0.0f)
         {
             //BR flip
-            fwdCross.x = -1.0f;
+            fwdCross.x = 1.0f;
             statusPtch = "BL";
         }
         else
         {
             statusPtch = string.Empty;
         }
-        currentSteerRequest.y = fwdCross.y;
-        currentSteerRequest.x = fwdCross.x;
+
+        currentSteerRequest.y = fwdCross.x;
+        currentSteerRequest.x = fwdCross.y;
 
         currentSteerRequest.x = Mathf.Clamp(currentSteerRequest.x, -1.0f, 1.0f);//pitch
         currentSteerRequest.y = Mathf.Clamp(currentSteerRequest.y, -1.0f, 1.0f);//yaw
-
-        accelerationRequest = 1.0f;
 
     }
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawSphere(target, 1.0f);
+        Gizmos.DrawSphere(target.position, 1.0f);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(transform.position, 1.0f);
 
         Gizmos.color = Color.blue;
 
-        Gizmos.DrawLine(pos, pos + (dir * acceptanceV));
+        Gizmos.DrawLine(transform.position, transform.position + (dir * 5.0f));
 
     }
+
 }
