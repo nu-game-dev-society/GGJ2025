8239695064d25f3c8e4f5c96a54ef237b370e97a
Joseph Whitehead <joseph.whitehead101@gmail.com>
2025-01-25 21:42:47 +0000

    rotating propellors

--- Assets/Scripts/CarController/CarController.cs
+++ Assets/Scripts/CarController/CarController.cs
@@ -10,6 +10,9 @@ public class CarController : MonoBehaviour
     private Animator[] propellorAnimators;
 
     [SerializeField]
+    private Transform[] propellorArms;
+
+    [SerializeField]
     private ParticleSystem exhaustParticleSystem;
 
     [SerializeField]
@@ -65,7 +68,9 @@ public class CarController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        this.ProcessInputs();
+        this.ProcessInputs(); 
+
+        UpdatePropellorRotation(inputs.accelerationRequest, inputs.currentSteerRequest.y);
     }
 
     private void FixedUpdate()
@@ -99,13 +104,30 @@ public class CarController : MonoBehaviour
         float maxSpeed = this.maxSpeedWithoutBoost + boostSpeed;
 
         this.currentSpeedRequest = maxSpeed * inputs.accelerationRequest;
-
+         
         foreach (Animator propellorAnimator in this.propellorAnimators)
         {
-            float oldPropellorSpeed = propellorAnimators[0].GetFloat("Speed");
-            float newPropellorSpeed = Mathf.Lerp(oldPropellorSpeed, this.currentSpeedRequest * Time.deltaTime * 10, Time.deltaTime);
+            float oldPropellorSpeed = propellorAnimators[0].GetFloat("Speed"); 
+
+            float newPropellorSpeed = Mathf.Lerp(oldPropellorSpeed, Mathf.Max(Mathf.Abs(this.currentSpeedRequest * 0.4f), Mathf.Abs(inputs.currentSteerRequest.y * steerRate.y * 2f)) * Time.deltaTime * 10, Time.deltaTime);
             propellorAnimator.SetFloat("Speed", newPropellorSpeed);
             propellorAudioSource.volume = newPropellorSpeed * 1f; 
         }
     }
+
+
+    public void UpdatePropellorRotation(float acceleration, float turn)
+    {
+        float newYRot = acceleration >= 0f ? 90f * turn : 180f - (90f * turn);
+
+        newYRot = newYRot < 0 ? 360 + newYRot : newYRot;
+
+        foreach (Transform propellorArm in propellorArms)
+        {
+            // Use Mathf.LerpAngle to ensure shortest path interpolation for angles
+            float y = Mathf.LerpAngle(propellorArm.localEulerAngles.y, newYRot, 2f * Time.deltaTime);
+            propellorArm.localEulerAngles = new Vector3(0f, y, 0f);
+        }
+    }
+
 }
